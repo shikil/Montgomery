908b0a3176f60e06fffd712fcc01f5ce49585d2f
shikil <noreply@email.com>
2020-11-06 15:41:59 +0800

    no return

--- src/Montgomery.jl
+++ src/Montgomery.jl
@@ -5,12 +5,12 @@ export MontgomeryCurve
 Base.:*(P::EllCrvPt, n::Int64) = n * P
 
 function (E::EllCrv{T})(a::T) where T
-	return E([a, root(a^3 + E.coeff[2] * a^2 + E.coeff[4] * a + E.coeff[5], 2)])
+	E([a, root(a^3 + E.coeff[2] * a^2 + E.coeff[4] * a + E.coeff[5], 2)])
 end
 
 function MontgomeryCurve(a::T) where T<:FieldElem
     aid = zero(parent(a))
-    return EllipticCurve([aid, a, aid, one(a), aid])
+    EllipticCurve([aid, a, aid, one(a), aid])
 end
 
 end # module
