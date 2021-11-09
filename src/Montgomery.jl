cd97c7995a191a4c7949219d1f8c0770fedd4b3c
shikil <467116+shikil@users.noreply.github.com>
2021-11-09 17:36:26 +0800

    further commit

--- src/Montgomery.jl
+++ src/Montgomery.jl
@@ -1,18 +1,23 @@
 module Montgomery
 
-import Base.:*
-import Hecke: FieldElem, EllipticCurve, EllCrv, EllCrvPt, ResidueRing, ZZ, j_invariant, parent, root
+import Hecke: EllipticCurve, EllCrv, EllCrvPt, ResidueRing, ZZ, ison_curve, j_invariant, order, parent, roots
 import Nemo: FlintFiniteField
-export FiniteField, MontgomeryCurve, j_invariant
 
-Base.:*(P::EllCrvPt, n::Int) = n * P
+export FiniteField, MontgomeryCurve, isogeny, j_invariant
 
-function (E::EllCrv{T})(a::T) where T
-	E([a, root(a^3 + E.coeff[2] * a^2 + E.coeff[4] * a + E.coeff[5], 2)])
+Base.:*(p::EllCrvPt, n::Int) = n * p
+
+function Base.in(coords::Tuple{T, T}, e::EllCrv{T}) where T
+	ison_curve(e, [coords[1], coords[2]])
 end
 
-function (E::EllCrv{T})(x::T, y::T) where T
-	E([x, y])
+function (e::EllCrv{T})(a::T) where T
+	_, y = parent(a)["y"]
+	e(a, roots(y^2 - (a^3 + e.coeff[2] * a^2 + e.coeff[4] * a + e.coeff[5]))[1])
+end
+
+function (e::EllCrv{T})(x::T, y::T) where T
+	e([x, y])
 end
 
 function FiniteField(p; gen="i")
@@ -20,9 +25,14 @@ function FiniteField(p; gen="i")
 	FlintFiniteField(t^2+1, gen)
 end
 
-function MontgomeryCurve(a::T) where T<:FieldElem
+function MontgomeryCurve(a::T) where T
     aid = zero(parent(a))
     EllipticCurve([aid, a, aid, one(a), aid])
+end
+
+function isogeny(p::EllCrvPt)
+	a = 2 * (1 - 2p.coordx^2)
+	MontgomeryCurve(a), x -> x * (p.coordx * x - 1)//(x - p.coordx)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,22 @@
 using Test
 using Montgomery
 
-k, i = FiniteField(431)
-Ea0 = MontgomeryCurve(329i+423)
+@testset "SIKE for beginners" begin
+    k, i = FiniteField(431)
+    Ea0 = MontgomeryCurve(329i + 423)
+    Pa = Ea0(100i + 248, 304i + 199)
+    Qa = Ea0(426i + 394, 51i + 79)
+    Pb = Ea0(358i + 275, 410i + 104)
+    Qb = Ea0(20i + 185, 281i + 239)
+    @test 2Pa == Pa * 2
+    @test (100i + 248, 304i + 199) in Ea0
 
-@test 2Ea0([100i+248, 304i+199]) == Ea0([100i+248, 304i+199]) * 2
-@test Ea0(100i+248) in [Ea0([100i+248, 304i+199]), -Ea0([100i+248, 304i+199])]
+    ka = 11
+    Sa = Pa + ka * Qa
+    @test Sa.coordx == 271i + 79 && Sa.coordy == 153i + 430
+    Ra = 8Sa
+    Ea1, ϕ = isogeny(Ra)
+    @test j_invariant(Ea1) == 107
+    @test ϕ(Pb.coordx) == 118i + 85 && ϕ(Qb.coordx) == 62i + 124 && ϕ(Sa.coordx) == 36i + 111
+    @test Ea1(118i + 85) in [Ea1(118i + 85, 274i + 150), -Ea1(118i + 85, 274i + 150)]
+end
