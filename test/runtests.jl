37a7a49b4de99e930131f8bbc1525068066635ca
shikil <467116+shikil@users.noreply.github.com>
2021-11-10 13:40:15 +0800

    Add issuper_singular

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using Montgomery
 
 @testset "SIKE for beginners" begin
+    # Public parameters
     k, i = FiniteField(431)
     Ea0 = MontgomeryCurve(329i + 423)
     Pa = Ea0(100i + 248, 304i + 199)
@@ -10,7 +11,9 @@ using Montgomery
     Qb = Ea0(20i + 185, 281i + 239)
     @test 2Pa == Pa * 2
     @test (100i + 248, 304i + 199) in Ea0
+    @test issuper_singular(Ea0)
 
+    # Alice's public key generation
     ka = 11
     Sa = Pa + ka * Qa
     @test Sa.coordx == 271i + 79 && Sa.coordy == 153i + 430
