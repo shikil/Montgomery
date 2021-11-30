e001b1f06a7811918050da2fb76a8e8f6fef9be5
shikil <467116+shikil@users.noreply.github.com>
2021-11-30 11:04:16 +0800

    Follow style guide

--- src/Montgomery.jl
+++ src/Montgomery.jl
@@ -1,8 +1,10 @@
 module Montgomery
 
-import AbstractAlgebra: FinFieldElem, Map, SetMap, codomain, elem_type
-import Hecke: EllipticCurve, EllCrv, EllCrvPt, ResidueRing, ZZ, base_field, characteristic, ison_curve, j_invariant, order, parent, roots
-import Nemo: FlintFiniteField
+import AbstractAlgebra: elem_type
+
+using AbstractAlgebra: FinFieldElem, Map, SetMap, codomain
+using Hecke: EllipticCurve, EllCrv, EllCrvPt, ResidueRing, ZZ, base_field, characteristic, ison_curve, j_invariant, order, parent, roots
+using Nemo: FlintFiniteField
 
 export FiniteImaginaryQuadraticField, Isogeny, MontgomeryCurve, codomain, degree, isisogenous, issuper_singular, j_invariant, order
 
@@ -35,12 +37,11 @@ elem_type(::Type{EllCrv{T}}) where T = EllCrvPt{T}
 
 Φ(x,y) = -x^2 * y^2 + x^3 + 1488x^2 * y + 1488x * y^2 + y^3 - 162000x^2 + 40773375x * y - 162000y^2 + 8748000000x + 8748000000y - 157464000000000
 
+(e::EllCrv{T})(x::T, y::T) where T = e([x, y])
 function (e::EllCrv{T})(a::T) where T
     _, y = parent(a)["y"]
     e(a, roots(y^2 - (a^3 + e.coeff[2] * a^2 + e.coeff[4] * a + e.coeff[5]))[1])
 end
-
-(e::EllCrv{T})(x::T, y::T) where T = e([x, y])
 
 function FiniteImaginaryQuadraticField(p; gen="i")
     _, t = ResidueRing(ZZ, p)["t"]
@@ -48,9 +49,7 @@ function FiniteImaginaryQuadraticField(p; gen="i")
 end
 
 MontgomeryCurve(a) = EllipticCurve(parent(a).([0, a, 0, 1, 0]))
-
 isisogenous(e1::EllCrv{T}, e2::EllCrv{T}) where T <: FinFieldElem = j_invariant(e1) == j_invariant(e2)
-
 issuper_singular(e::EllCrv{T}) where T <: FinFieldElem = order(e) % characteristic(base_field(e)) == 1
 
 end # module
